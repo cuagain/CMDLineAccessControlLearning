08c68fbe8781e333a89b25527d7cd0c15fbd639a
Thanawat Soisakhoo <chaicsxmx2@hotmail.com>
2014-11-02 22:58:11 +0700

    Initial Commit

---
+++ CMDLineAccessControlLearning/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CMDLineAccessControlLearning
+//
+//  Created by Thanawat Soisakhoo on 11/2/2557 BE.
+//  Copyright (c) 2557 Thanawat Soisakhoo. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
